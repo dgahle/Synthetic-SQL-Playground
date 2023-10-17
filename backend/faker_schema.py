35f425b4017c8e9d6996a894ff43229c1633782c
Daljeet Gahle <Daljeet.Gahle@ukhsa.gov.uk>
2023-10-17 10:16:30 +0100

    Added backend/faker_schema.py to translate a schema CSV to fake data using faker.

---
+++ backend/faker_schema.py
@@ -0,0 +1,33 @@
+from faker import Faker
+
+
+class FakerSchema(object):
+
+    def __init__(self, faker=None, locale=None, providers=None, includes=None):
+        self._faker = faker or Faker(locale=locale, providers=providers, includes=includes)
+
+    def generate_fake(self, schema, iterations=1):
+        result = [self._generate_one_fake(schema) for _ in range(iterations)]
+        return result[0] if len(result) == 1 else result
+
+    def _generate_one_fake(self, schema):
+        """
+        Recursively traverse schema dictionary and for each "leaf node", evaluate the fake
+        value
+
+        Implementation:
+        For each key-value pair:
+        1) If value is not an iterable (i.e. dict or list), evaluate the fake data (base case)
+        2) If value is a dictionary, recurse
+        3) If value is a list, iteratively recurse over each item
+        """
+        data = {}
+        for k, (v, kwargs) in schema.items():
+            if isinstance(v, dict):
+                data[k] = self._generate_one_fake(v)
+            elif isinstance(v, list):
+                data[k] = [self._generate_one_fake(item) for item in v]
+            else:
+                data[k] = getattr(self._faker, v)(**kwargs)
+
+        return data
